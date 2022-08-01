98c563e054ab91b0485f3c691300327cab21382c
J1I2B345 <juan.bevilacqua@outlook.com>
2022-08-01 14:21:27 -0300

    express/db

---
+++ Api/index.js
@@ -0,0 +1,17 @@
+const serverExpress = require ('./src/app');
+const { conn } = require ('./src/db');
+const {Server} = require ("socket.io");
+const http = require ('http');
+// const sockets = require ('./src/sockets')
+
+// let httpServer
+// let io
+conn.sync({ force: true }).then(() => {
+    const server= http.createServer(serverExpress)
+    let httpServer = server.listen(process.env.PORT, () => {
+        console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
+    });
+    let io = new Server(httpServer)
+//    sockets(io)
+    
+});
