40ab629c7e2d029a34c98ae33f73f524e8a18281
Ulysses <94791119+Blob2142@users.noreply.github.com>
2022-08-09 15:02:35 -0300

    Update reviewsRutes.js

--- Api/src/routes/reviewsRutes.js
+++ Api/src/routes/reviewsRutes.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 const ReviewSchema = require("../models/Reviews")
-const UserSchema = require("../models/Users")
 
 //----------------POST USER REVIEW------------------------
 router.post("/", async (req, res) => {
@@ -15,10 +14,35 @@ router.post("/", async (req, res) => {
         res.json("creo que malio siel");
     }
     catch (e) {
-        console.log(e)
         res.json("Algo Malio Sal :O").status(404)
     }
 })
 
+//----------------PUT USER REVIEW------------------------
+router.put("/", async (req, res) => {
+    const { userRated, reviewer, qualification, comment } = req.body;
+    try {
+        if(!userRated, !reviewer, !qualification) {
+            return res.json("te faltan datos pa")
+        }
+        const reviewPut = await ReviewSchema.findOneAndUpdate({userRated: userRated,reviewer: reviewer}, req.body);
+        reviewPut.save()
+        res.json("creo que malio siel");
+    }
+    catch (e) {
+        res.json("Algo Malio Sal :O").status(404)
+    }
+})
+//----------------DELETE USER REVIEW------------------------
+router.delete("/:id", async(req, res) => {
+    const { id } = req.params;
+    try {
+        const reviewPut = await ReviewSchema.findByIdAndDelete(id);
+        res.json("creo que malio siel");
+    }
+    catch (e) {
+        res.json("Algo Malio Sal :O").status(404)
+    }
+})
 
 module.exports = router;
