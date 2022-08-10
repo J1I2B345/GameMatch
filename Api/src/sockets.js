a44a0dd904053420bc3ca53f8f29a23dd96938ec
J1I2B345 <juan.bevilacqua@outlook.com>
2022-08-10 17:57:17 -0300

    fixing

--- Api/src/sockets.js
+++ Api/src/sockets.js
@@ -15,11 +15,11 @@ module.exports = (io) => {
         })
 
         socket.on('disconnect', () =>{
-            socket.broadcast.to(global[socket.id].game).emit('message', `${global[socket.id].username} has left the chat`)
+            console.log(global[socket.id])
+            // socket.broadcast.to(global[socket.id].game).emit('message', `${global[socket.id].username} has left the chat`)
             leaveRoom( global[socket.id].game, global[socket.id]._id)
             io.to(global[socket.id].game).emit('gameUsers', getGameUsers(global[socket.id].game))
         })
-        
     }
     
    
