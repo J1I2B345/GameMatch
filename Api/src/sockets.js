488ee6138d691152a19fe96fd56a894b0b70817c
J1I2B345 <juan.bevilacqua@outlook.com>
2022-08-10 17:25:34 -0300

    probando global

--- Api/src/sockets.js
+++ Api/src/sockets.js
@@ -8,7 +8,7 @@ module.exports = (io) => {
         socket.on('joinRoom', (user)=>{
             if(user.username){
             let userFull = {...user, socketid: socket.id}
-            global.user = userFull
+            global[socket.id] = userFull
             userJoin(userFull)
             socket.join(userFull.game)
             socket.broadcast.to(userFull.game).emit('message', `${userFull.username} has joined the chat`)
@@ -19,7 +19,7 @@ module.exports = (io) => {
         })
 
         socket.on('disconnect', () =>{
-           console.log (global.user)
+           console.log (global[socket.id])
             
             console.log(`${socket.id} left the room`)
             // leaveRoom( user.game, user._id)
