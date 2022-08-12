4f5000df3a356da2fcc8f5251f0a1c0d1b9accbb
moisesdurandelrazo <moises6737@gmail.com>
2022-08-12 10:39:16 -0500

    update

--- Client/src/components/GameHome.jsx
+++ Client/src/components/GameHome.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants';
-import Nav from './Nav';
-import GameCard from './GameCard';
+import React from "react";
+import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import Nav from "./Nav";
+import GameCard from "./GameCard";
 
 const GameHome = () => {
-     return (
-          <View
-               style={{
-                    alignItems: 'center',
-                    height: '100%',
-               }}
-          >
-               <Text
-                    style={{
-                         marginTop: Constants.statusBarHeight + 10,
-                         marginBottom: 10,
-                         color: 'white',
-                         fontSize: 40,
-                    }}
-               >
-                    Select your game
-               </Text>
-               <GameCard />
-               <Nav />
-               <StatusBar style="auto" />
-          </View>
-     );
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <Text
+        style={{
+          marginTop: Constants.statusBarHeight + 10,
+          marginBottom: 10,
+          color: "white",
+          fontSize: 40,
+        }}
+      >
+        Select your game
+      </Text>
+      <GameCard />
+      <Nav />
+      <StatusBar style="auto" />
+    </View>
+  );
 };
 
 export default GameHome;
--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,39 +1,38 @@
-import { Routes, Route } from 'react-router-native';
-import { View } from 'react-native';
-import Login from './Login.jsx';
-import SelectGame from './SelectGame.jsx';
-import RoomLoL from './RoomLoL.jsx';
-import RoomCS from './RoomCS.jsx';
-import RoomR6 from './RoomR6.jsx';
-import Form from './Form.jsx';
-import Chat from './Chat.jsx';
-import CreateGame from './CreateGame.jsx';
-import Profile from './Profile.jsx';
-import WhyPremium from './WhyPremium.jsx';
-import BuyPremium from './BuyPremium.jsx';
-import News from './News.jsx';
+import { Routes, Route } from "react-router-native";
+import { View } from "react-native";
+import Login from "./Login.jsx";
+import SelectGame from "./SelectGame.jsx";
+import RoomLoL from "./RoomLoL.jsx";
+import RoomCS from "./RoomCS.jsx";
+import RoomR6 from "./RoomR6.jsx";
+import Form from "./Form.jsx";
+import Chat from "./Chat.jsx";
+import CreateGame from "./CreateGame.jsx";
+import Profile from "./Profile.jsx";
+import WhyPremium from "./WhyPremium.jsx";
+import BuyPremium from "./BuyPremium.jsx";
+import News from "./News.jsx";
 
 const Home = () => {
-    return (
-        <View>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route exact path="/selectgame" element={<SelectGame />} />
-                <Route path="form/:id" element={<Form />} />
-                <Route path="/createGame" element={<CreateGame />} />
-                <Route path="/playersLoL" element={<RoomLoL />} />
-                <Route path="/playersCS" element={<RoomCS />} />
-                <Route path="/playersR6" element={<RoomR6 />} />
-                <Route path="selectchat" element={<SelectChat />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/whypremium" element={<WhyPremium />} />
-                <Route path="/buypremium" element={<BuyPremium />} />
-                <Route path="/news" element={<News />} />
-            </Routes>
-        </View>
-    );
->>>>>>> 666658aeedba818ab199e53cb8ae7aec7d0e8065
+  return (
+    <View>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route exact path="/selectgame" element={<SelectGame />} />
+        <Route path="form/:id" element={<Form />} />
+        <Route path="/createGame" element={<CreateGame />} />
+        <Route path="/playersLoL" element={<RoomLoL />} />
+        <Route path="/playersCS" element={<RoomCS />} />
+        <Route path="/playersR6" element={<RoomR6 />} />
+        <Route path="selectchat" element={<SelectChat />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/whypremium" element={<WhyPremium />} />
+        <Route path="/buypremium" element={<BuyPremium />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </View>
+  );
 };
 
 export default Home;
