154a6816e42c6d31a022e273d2283a6e77c56d5a
moisesdurandelrazo <moises6737@gmail.com>
2022-08-12 10:37:32 -0500

    show games

--- Client/gm-app/src/Components/GamesCard/GameCard.jsx
+++ Client/gm-app/src/Components/GamesCard/GameCard.jsx
@@ -1,42 +1,37 @@
-import React from 'react';
-import { FlatList, View, Image } from 'react-native';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-native';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const GameCard = () => {
-     let [gamesDB, setGames] = useState([]);
+  let [gamesDB, setGames] = useState([]);
 
-     const fetchGames = async () => {
-          try {
-               const response = await axios.get('https://backend-gamematch.herokuapp.com/games');
-               const respuesta = response.data;
-               setGames(respuesta);
-          } catch (error) {
-               console.error(error.message);
-          }
-     };
+  const fetchGames = async () => {
+    try {
+      const response = await axios.get(
+        "https://backend-gamematch.herokuapp.com/games"
+      );
+      const respuesta = response.data;
+      setGames(respuesta);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-     useEffect(() => {
-          fetchGames();
-     }, []);
+  useEffect(() => {
+    fetchGames();
+  }, []);
 
-     return (
-          <FlatList
-               style={{ marginBottom: 45 }}
-               data={gamesDB}
-               renderItem={({ item: game }) => (
-                    <View key={game.id} style={{ margin: 5 }}>
-                         <Link to={game.link} activeOpacity={1} underlayColor={''}>
-                              <Image
-                                   source={{ uri: game.image }}
-                                   style={{ width: 340, height: 180, borderRadius: 20 }}
-                              />
-                         </Link>
-                    </View>
-               )}
-          />
-     );
+  console.log({ fetchGames });
+  console.log({ gamesDB });
+
+  return (
+    <div>
+      {gamesDB.map((data) => (
+        <img className="img-game" src={data.image} alt="" key={data.id} />
+      ))}
+    </div>
+  );
 };
 
 export default GameCard;
