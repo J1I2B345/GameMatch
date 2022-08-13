d99a904e835149240aefe02f26f94b7304521517
J1I2B345 <juan.bevilacqua@outlook.com>
2022-08-12 23:54:37 -0300

    trying cors with *

--- Api/src/app.js
+++ Api/src/app.js
@@ -14,7 +14,7 @@ serverExpress.use(morgan('dev'));
 serverExpress.use(express.json())
 
 serverExpress.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:19006'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
