6446af1aeb4fa84b0a54c62c159e2be8944044d1
J1I2B345 <juan.bevilacqua@outlook.com>
2022-09-07 15:38:50 -0300

    fixing login

--- Client/src/components/Register.jsx
+++ Client/src/components/Register.jsx
@@ -36,11 +36,12 @@ const reviewSchema = yup.object({
 const Register = () => {
 	const dispatch = useDispatch();
 	const navigation = useNavigate();
-	useEffect(() => {
-		dispatch(allUser());
-	}, []);
 
-	const user = useSelector((state) => state.games.aux);
+	// useEffect(() => {
+	// 	dispatch(allUser());
+	// }, []);
+
+	// const user = useSelector((state) => state.games.aux);
 	// console.log(user[0])
 	const setpassword = (value) => {
 		setConfirm(value);
@@ -50,20 +51,20 @@ const Register = () => {
 		//console.log(values);
 		try {
 			if (values) {
-				if (user.map((d) => d.email).includes(values.email)) {
-					Alert.alert("The email already exists");
-					return;
-				}
-				if (user.map((d) => d.username).includes(values.username)) {
-					Alert.alert("The username already exists, try again");
-					return;
-				}
+				// if (user.map((d) => d.email).includes(values.email)) {
+				// 	Alert.alert("The email already exists");
+				// 	return;
+				// }
+				// if (user.map((d) => d.username).includes(values.username)) {
+				// 	Alert.alert("The username already exists, try again");
+				// 	return;
+				// }
 
-				if (confirm !== values.password) {
-					Alert.alert("The password dont match, try again");
-					return;
-				}
-				dispatch(register(values));
+				// if (confirm !== values.password) {
+				// 	Alert.alert("The password dont match, try again");
+				// 	return;
+				// }
+				// dispatch(register(values));
 				await axios.post(
 					"https://backend-gamematch.herokuapp.com/users/register",
 					values
@@ -76,8 +77,9 @@ const Register = () => {
 				Alert.alert("💖Welcome to GameMatch!!🎮");
 			}
 		} catch (error) {
-			//Alert.alert(error.message);
-			console.log({ message: error.message });
+			Alert.alert(error.response.data.message);
+			alert(error.response.data.message);
+			console.log({ message: error.response.data.message });
 		}
 	};
 
